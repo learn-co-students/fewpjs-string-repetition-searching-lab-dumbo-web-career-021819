f592bf424d50e235c70284efca93ecb6e40bcc93
James Dorr <jamesdorr3@gmail.com>
2019-04-06 19:04:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+// function heyJude() {
+//     return 'Na na na na na na na, na na na na, hey Jude.'.repeat(16)
+// }
+
+const heyJude = () => 'Na na na na na na na, na na na na, hey Jude.'.repeat(16);
+
+// function prohibitedLanguage(username) {
+//     return username.search(/candycorn|brusselssprouts/)
+// }
+
+const prohibitedLanguage = username => username.search(/candycorn|brusselssprouts/);
