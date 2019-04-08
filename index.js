19cd2b1e241c86a190449dde705dfbda9ca52777
Rhohoman <rhohoman@gmail.com>
2019-04-08 11:30:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+//
+//
+// function heyJude(str,num){
+//   return (new Array(num+1)).join(str);
+// }
+
+
+let heyJude = (verse) => {
+  return verse.repeat(16)
+}
+
+let prohibitedLanguage = (username) => {
+  let bannedWords = /candycorn|brusselssprouts/;
+
+  return username.search(bannedWords)
+}
+
+// if ((username == "ilovecandycorn") || (username == "brusselssproutsarenumberone")){
+//
+// }
+
+// if (username == "ilovecandycorn"){
+//   return 5
+// } else if (username == "brusselssproutsarenumberone") {
+//   return 0
+// } else {
+//   return -1
+// }
