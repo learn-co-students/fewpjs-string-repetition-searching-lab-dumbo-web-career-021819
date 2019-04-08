b5e6069e74ca12d64d9e80eec449b5e289db8cff
Jaela Wesley <github email address>
2019-04-07 23:36:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude() {
+    return "Na na na na na na na, na na na na, hey Jude.".repeat(16) 
+}
+
+function prohibitedLanguage(username) {
+    let myRegEx = /candycorn|brusselssprouts/;
+    return username.search(myRegEx);
+
+}
